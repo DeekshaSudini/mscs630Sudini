a3e79189559654475ff1a9b73e48f35c380416ce
DeekshaSudini <35581434+DeekshaSudini@users.noreply.github.com>
2018-02-17 14:53:15 -0500

    Update Driver.java

--- labs/1/Driver.java
+++ labs/1/Driver.java
@@ -1,14 +1,22 @@
+/**
+   * file: Driver.java
+   * author: Deeksha Sudini
+   * course: mscs630
+   * assignment: lab 1
+   * due date: January 24, 2018
+   * version: 9.0.4
+   * 
+   * This file converts the plain text to cipher text .
+   */
 import java.util.*;
 import java.io.*;
-
-class Driver
-{
+//The Driver class takes the plain text as input string and maps it to int. 
+class Driver{
   public static void main(String[] args) throws FileNotFoundException
-{
-  System.out.println("Please Enter the String: ");
-  Scanner sc = new Scanner(new File("input.txt"));
-  while (sc.hasNext())
-    {
+  {
+    System.out.println("Please Enter the String: ");
+    Scanner sc = new Scanner(new File("input.txt"));
+    while (sc.hasNext()){
       String inputText = sc.nextLine();
       inputText = inputText.toUpperCase();
       int[] nums =str2int(inputText);
@@ -16,20 +24,18 @@ class Driver
         System.out.print(nums[j]+" ");
       System.out.println();
     }
-}
-  public static int[] str2int(String plainText)
-{
-  int[] temp = new int[plainText.length()] ;
-  for (int i=0;i<plainText.length();i++)
-  {
-    char c = plainText.charAt(i);
-    if(c == 32)
-      temp[i]=26;
-    else
-      temp[i]= ((int) c) - 65;
   }
-  return temp;
-}
+  public static int[] str2int(String plainText){
+    int[] temp = new int[plainText.length()] ;
+    for (int i=0;i<plainText.length();i++){
+      char c = plainText.charAt(i);
+      if(c == 32)
+      temp[i]=26;
+      else
+      temp[i]= ((int) c) - 65;
+    }
+    return temp;
+  }
 }
 
 
